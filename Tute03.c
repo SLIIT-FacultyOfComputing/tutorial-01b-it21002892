7e3464af0fa745676333932eb1a1fc8b1406dabe
IT21002892 <87407695+it21002892@users.noreply.github.com>
2021-07-14 16:43:32 +0000

    I have completed three questions.

--- Tute03.c
+++ Tute03.c
@@ -14,6 +14,17 @@ sum = 1+2+3+...+10 = 55 */
 #include <stdio.h>
 int main() {
   
+  int n , sum = 0 , i;
+
+	printf("Enter a value for 'n' : ");
+	scanf("%d" , &n);
+
+	for (i = 1 ; i <= n ; i++)
+	{
+		sum += i;
+	}
+
+	printf("sum = %d\n" , sum);
   return 0;
 }
 
--- Tute04.c
+++ Tute04.c
@@ -4,16 +4,58 @@ Implement the three functions minimum(), maximum() and multiply() below the main
 
 Do not change the code given in the main() function when you are implementing your solution.*/
 
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-   int no1, no2;
-   printf("Enter a value for no 1 : ");
-   scanf("%d", &no1);
-   printf("Enter a value for no 2 : ");
-   scanf("%d", &no2);
-   printf("%d ", minimum(no1, no2));
-   printf("%d ", maximum(no1, no2));
-   printf("%d ", multiply(no1, no2));
-   return 0;
+int minimum(int number1 ,int number2);
+int maximum(int number1 ,int number2);
+int multiply(int number1 ,int number2);
+
+int main (void)
+{
+	int no1 , no2;
+
+	printf("Enter a Value for no 1 : ");
+	scanf("%d" , &no1);
+
+	printf("Enter a Value for no 2 : ");
+	scanf("%d" , &no2);
+
+	printf("%d " , minimum(no1 , no2));
+	printf("%d " , maximum(no1 , no2));
+	printf("%d " , multiply(no1 , no2));
+
+	return 0;
+}
+
+int minimum(int number1 ,int number2)
+{
+	if (number1 >= number2)
+	{
+		return number2;
+	}
+	else
+	{
+		return number1;
+	}
+}
+
+int maximum(int number1 ,int number2)
+{
+	if (number1 <= number2)
+	{
+		return number2;
+	}
+	else
+	{
+		return number1;
+	}
+}
+
+int multiply(int number1 ,int number2)
+{
+	int answer;
+
+	answer = number1 * number2;
+
+	return answer;
 }
