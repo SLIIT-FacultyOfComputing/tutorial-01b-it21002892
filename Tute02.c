1f3c6aa61a2a85a8b8a6cf6136275a8242d4e7ec
IT21002892 <87407695+it21002892@users.noreply.github.com>
2021-07-14 16:31:58 +0000

    I have completed two questions

--- Tute02.c
+++ Tute02.c
@@ -20,6 +20,22 @@ Amount = 30 x 50 + (50-30) x 40 = 2300*/
 
 int main() {
 
+float distance , amount = 0;
+
+	printf("Enter the Distance : ");
+	scanf("%f" , &distance);
+
+	if (distance <= 30)
+	{
+		amount = distance * 50;
+	}
+	else
+	{
+		amount = (30 * 50) + ( (distance - 30) * 40);
+	}
+
+	printf("\n");
+	printf("Amount : %.2f\n" , amount);
   
   return 0;
 }
